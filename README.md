d3e44cbe14aa92de6cb8dc5d8132ae09374b9d1d
JaAy27 <jnax27@hotmail.com>
2023-02-01 17:57:25 -0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# What is this project?
+It extracts NASA's Astronomy Photo of the Day and deploys it to Streamlit.
---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+import streamlit as st
+
+# Prepare the API key and API url
+api_key = "VPIXkKf3JmUEbTG4LcduiwEVqaq2Keovw5x25MGS"
+url = "https://api.nasa.gov/planetary/apod?api_key=VPIXkKf3JmUEbTG4LcduiwEVqaq2Keovw5x25MGS"
+
+
+# Get the request data as a dictionary
+request = requests.get(url)
+content = request.json()
+
+# Extract the image title, url and explanation
+title = content["title"]
+img_url = "https://api.nasa.gov/assets/img/general/apod.jpg"
+explanation = content["explanation"]
+
+
+# Download the image
+image_filepath = "image.jpg"
+response = requests.get(img_url)
+with open(image_filepath, "wb") as file:
+    file.write(response.content)
+
+
+st.title(title)
+st.image(img_url)
+st.write(explanation)
+
+
